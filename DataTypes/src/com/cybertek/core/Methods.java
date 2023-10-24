22e3dc935cad67993137672024f43a7249975b3d
baiazbekova <baiazbekova@gmail.com>
2023-10-24 12:57:59 -0400

    arraylist

---
+++ DataTypes/src/com/cybertek/core/Methods.java
@@ -0,0 +1,27 @@
+package com.cybertek.core;
+
+public class Methods {
+
+    public void methodA(){
+        System.out.println("methodA() is called");
+
+    }
+
+    public void methodB(String str){
+        System.out.println("methodB(String str) is called");
+    }
+
+    public String methodC(int x){
+        return "methodC(int x) is called";
+    }
+
+    public static void methodD(){
+        System.out.println("methodD() is called");
+    }
+
+    public String methodE(){
+        return "methodE() is called";
+    }
+
+
+}
